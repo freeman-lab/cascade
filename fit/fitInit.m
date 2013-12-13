d38e261ad293fc45234797f0bdb2255b0c259b4c
freeman <the.freeman.lab@gmail.com>
2013-12-13 00:12:20 -0500

    Explicit computation of stats if not provided

--- fit/fitInit.m
+++ fit/fitInit.m
@@ -13,9 +13,6 @@ fit.cv.n = 5;
 fit.cv.obj = cvpartition(d.k,'kfold',fit.cv.n);
 fit.n = n;
 fit.type = fitType;
-if isfield(d,'stats')
-fit.stats = d.stats;
-end
 if isfield(d,'stims')
 fit.stims = d.stims;
 end
@@ -25,6 +22,18 @@ fit.c = d.c;
 fit.samp = d.samp;
 fit.rnd.n = d.rnd.n;
 fit.boot.n = 0;
+if isfield(d,'stats')
+	fit.stats = d.stats;
+else
+	for ic = 1:fit.c
+		d.stats.mn(ic) = nanmean(vector(d.S_ctk(ic,:,:)));
+		d.stats.sd(ic) = nanstd(vector(d.S_ctk(ic,:,:)));
+		d.stats.prc(ic,1) = min(vector(d.S_ctk(ic,:,:)));
+		d.stats.prc(ic,2) = max(vector(d.S_ctk(ic,:,:)));
+		fit.stats = d.stats;
+	end
+end
+
 
 switch fitType
 
@@ -33,15 +42,7 @@ switch fitType
 		fit.q = fit.n*fit.c;
 		fit.B_q = ones(fit.q,1)/5;
 		fit.pr.B.L = kron(eye(fit.c),secondDeriv(fit.q/fit.c));
-		fit.pr.B.sigma = 0.05;
-		switch fit.error
-		case 'mse'
-			fit.g.type = 'linear';
-			fit.g.p = [0];
-		case 'loglik'
-			fit.g.type = 'logexp1';
-			fit.g.p = [0 1];
-		end
+		fit.pr.B.sigma = 0.005;
 
 	% set params for nonlinear-linear model
 	case 'NL'
@@ -60,11 +61,18 @@ switch fitType
 			fit.f(ic).width = (fit.f(ic).mxVal-fit.f(ic).mnVal)/(fit.f(ic).m-1);
 			fit.f(ic).w = fit.f(ic).nd' + randn(size(fit.f(ic).nd'));
 		end
-		fit.g.type = 'linear';
-		fit.g.p = [0];
 		fit.pr.B.L = kron(eye(fit.c),secondDeriv(fit.q/fit.c));
 		fit.pr.B.sigma = 0.01;
 		fit.pr.F.L = kron(eye(fit.c),secondDeriv(fit.m));
 		fit.pr.F.sigma = 0.01;
 	
 end
+
+switch fit.error
+		case 'mse'
+			fit.g.type = 'linear';
+			fit.g.p = [0];
+		case 'loglik'
+			fit.g.type = 'logexp1';
+			fit.g.p = [0 1];
+		end
